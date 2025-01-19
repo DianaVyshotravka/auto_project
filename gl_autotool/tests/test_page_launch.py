38fa6bf433bc52af454a85f61722497d608d369f
DianaVyshotravka <vyshotravka.diana2@gmail.com>
2025-01-19 16:07:18 +0200

    moved fixture to conftest.py

--- gl_autotool/tests/test_page_launch.py
+++ gl_autotool/tests/test_page_launch.py
@@ -7,11 +7,11 @@ class TestLaunchPage(BaseTest):
     Test class for verifying web page loading and title.
     """
 
-    def test_page_load(self):
+    def test_page_load(self, setup_and_teardown):
         """
         Test to verify if web page is loaded and gets its title.
         """
-        page = BasePage(self.page)
+        page = BasePage(setup_and_teardown)
         page.open_url(self.base_url)
         title = page.get_title()
 
