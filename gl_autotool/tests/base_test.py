7fade969d92896d6d8980c8688e2efb7c3aee21e
DianaVyshotravka <vyshotravka.diana2@gmail.com>
2025-01-22 23:31:39 +0200

    Revert "moved fixture to conftest.py"
    This reverts commit 38fa6bf433bc52af454a85f61722497d608d369f.

--- gl_autotool/tests/base_test.py
+++ gl_autotool/tests/base_test.py
@@ -1,4 +1,32 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
 
 class BaseTest:
     base_url = "https://ultimateqa.com/automation"
     default_browser = "chromium"
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self, request: pytest.FixtureRequest):
+        """
+        Fixture for setting up and tearing down Playwright resources.
+
+        Parameters
+        ----------
+        request: The request object which allows access to test-specific parameters,
+        including the browser type specified when running the test.
+        """
+        browser_type = getattr(request, "param", self.default_browser)
+        self.playwright = sync_playwright().start()
+
+        if browser_type == "chromium":
+            self.browser = self.playwright.chromium.launch(headless=False)
+        else:
+            raise ValueError(f"Unsupported browser type: {browser_type}")
+
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
+
+        yield
+
+        self.browser.close()
+        self.playwright.stop()
--- gl_autotool/tests/test_page_launch.py
+++ gl_autotool/tests/test_page_launch.py
@@ -7,11 +7,11 @@ class TestLaunchPage(BaseTest):
     Test class for verifying web page loading and title.
     """
 
-    def test_page_load(self, setup_and_teardown):
+    def test_page_load(self):
         """
         Test to verify if web page is loaded and gets its title.
         """
-        page = BasePage(setup_and_teardown)
+        page = BasePage(self.page)
         page.open_url(self.base_url)
         title = page.get_title()
 
